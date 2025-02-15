a7c562db808b74ea2a31563a6b7f4e3188ba16f2
Jide Tireni <jtirenipraise@gmail.com>
2025-02-15 14:11:16 +0100

    router.go

--- api/router.go
+++ api/router.go
@@ -3,7 +3,6 @@ package api
 import (
 	"context"
 	"log/slog"
-	"net"
 	"net/http"
 	"sync"
 	"time"
@@ -17,7 +16,7 @@ type ApiServer struct {
 	logger *slog.Logger
 }
 
-func New(conf *config.Config, logger *slog.Logger) *ApiServer {
+func NewApi(conf *config.Config, logger *slog.Logger) *ApiServer {
 	return &ApiServer{
 		conf:   conf,
 		logger: logger,
@@ -34,30 +33,41 @@ func (s *ApiServer) ping(c *gin.Context) {
 func (s *ApiServer) Start(ctx context.Context) error {
 	router := gin.Default()
 	router.GET("/ping", s.ping)
+	middle := NewLoggerMiddleware(s.logger)
+	router.Use(middle)
 
 	// if err := router.Run(s.conf.ApiServerHost); err != nil {
 	// 	s.logger.Error("Failed to run server", "error", err)
 	// }
 
 	srv := &http.Server{
-		Addr:    net.JoinHostPort(s.conf.ApiServerHost, s.conf.ApiServerPort),
+		Addr:    ":8080",
 		Handler: router,
 	}
+
 	go func() {
-		s.logger.Info("api server", "port", s.conf.ApiServerPort)
+		s.logger.Info("apiserver", "port", 8080)
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			s.logger.Error("Failed to ListenAndServe:", "error", err)
 		}
 	}()
 
+	// Shutdown logic
 	var wg sync.WaitGroup
+
+	// This increments the WaitGroup counter by 1, indicating that one goroutine
+	// is about to start.
 	wg.Add(1)
 	go func() {
+		// ensures that wg.Done() is called when the goroutine exits
+		// wg.Done() decrements the WaitGroup counter by 1
 		defer wg.Done()
-		<-ctx.Done()
+		<-ctx.Done() // used to signal cancellation or timeout. When the context is canceled
 
+		//This creates a new context (shutdownctx) with a timeout of 10 seconds
 		shutdownctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
+
 		if err := srv.Shutdown(shutdownctx); err != nil {
 			s.logger.Error("api server failed to shutdown", "error", err)
 		}
